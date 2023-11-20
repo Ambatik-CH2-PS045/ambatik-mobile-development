94395f49ccb9a314de9e646c20cddc969024d944
Austin Lieandro <austinliecoeg123@gmail.com>
2023-11-20 16:19:44 +0700

    Welcome Page + Add file login and register screen

--- app/src/main/java/com/example/ambatik/AmbatikApp.kt
+++ app/src/main/java/com/example/ambatik/AmbatikApp.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
