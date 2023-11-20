704db06c35183620afbb77bb7b2dba7df28fcee5
Austin Lieandro <austinliecoeg123@gmail.com>
2023-11-20 11:26:15 +0700

    Welcome Page

---
+++ app/src/main/java/com/example/ambatik/AmbatikApp.kt
@@ -0,0 +1,101 @@
+package com.example.ambatik
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.ambatik.ui.theme.AmbatikTheme
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun AmbatikApp(
+){
+    Surface(
+        color = Color(0xFF282A37)
+    ){
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(20.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = "Foto Login",
+                modifier = Modifier
+                    .size(250.dp, 250.dp)
+            )
+            Text(
+                text = "Selamat datang di Ambatik",
+                color = Color.White,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(8.dp)
+            )
+            Text(
+                text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ipsum orci, aliquam non leo non, laoreet blandit sem. Nunc nec.",
+                color = Color(0xFF79869F),
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .padding(10.dp, 0.dp, 10.dp, 8.dp)
+            )
+            Button(
+                shape = RoundedCornerShape(10.dp),
+                colors = ButtonDefaults.buttonColors(Color(0xFF1D90F4)),
+                onClick = { /*Ke halaman login*/ },
+                modifier = Modifier
+                    .size(327.dp, 55.dp)
+            ) {
+                Text(text = "Login")
+            }
+            Button(
+                shape = RoundedCornerShape(10.dp),
+                colors = ButtonDefaults.buttonColors(Color(0xFF282A37)),
+                border = BorderStroke(1.dp, Color(0xFF1D90F4)),
+                onClick = { /*Ke halaman register*/ },
+                modifier = Modifier
+                    .size(327.dp, 55.dp)
+                    .padding(0.dp, 8.dp, 0.dp, 0.dp)
+            ) {
+                Text(
+                    text = "Register",
+                    color = Color(0xFF1D90F4),
+                )
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun AmbatikAppPreview(){
+    AmbatikTheme {
+        AmbatikApp()
+    }
+}
