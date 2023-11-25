dc02b9af21b8955674186694cdcd34a688a3a1ee
Austin Lieandro <austinliecoeg123@gmail.com>
2023-11-25 20:01:53 +0700

    Can View Article

--- app/src/main/java/com/example/ambatik/ui/screen/articel/ArticelScreen.kt
+++ app/src/main/java/com/example/ambatik/ui/screen/articel/ArticelScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -65,7 +67,9 @@ fun ArticelScreen(
                 verticalArrangement = Arrangement.spacedBy(16.dp),
             ){
                 items(articleListState.value ?: emptyList()){ data ->
-                    Card{
+                    Card(
+                        colors = CardDefaults.cardColors(Color.Gray)
+                    ){
                         ArticleItem(
                             image = data.urlBanner,
                             title = data.title,
