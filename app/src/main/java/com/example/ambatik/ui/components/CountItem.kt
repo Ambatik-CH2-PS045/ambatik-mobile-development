2cfc29aa8ac28e574285d67b99f0721d0a7d5b4c
Austin Lieandro <72506433+austinlieandro@users.noreply.github.com>
2023-11-29 21:04:08 +0700

    Shop and Detail Shop

--- app/src/main/java/com/example/ambatik/ui/components/CountItem.kt
+++ app/src/main/java/com/example/ambatik/ui/components/CountItem.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.size.Dimension
 import com.example.ambatik.ui.theme.AmbatikTheme
 
 @Composable
@@ -34,9 +35,7 @@ fun CountItem(
     Row(
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier
-            .size(width = 110.dp, height = 40.dp)
-            .padding(4.dp)
+        modifier = modifier.size(width = 110.dp, height = 40.dp).padding(4.dp)
     ) {
         Surface(
             shape = RoundedCornerShape(size = 5.dp),
@@ -45,22 +44,16 @@ fun CountItem(
             contentColor = MaterialTheme.colorScheme.primary,
             modifier = Modifier.size(30.dp)
         ) {
-            Box(
-                contentAlignment = Alignment.Center,
-                modifier = modifier
-                    .fillMaxSize(1.0f)
-            ){
-                Text(
-                    text = "—",
-                    fontSize = 20.sp,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier
-                        .align(Alignment.Center)
-                        .clickable {
-                            onProductDecreased(orderId)
-                        },
-                )
-            }
+            Text(
+                text = "—",
+                fontSize = 22.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .weight(1f)
+                    .clickable {
+                        onProductDecreased(orderId)
+                    }
+            )
         }
         Text(
             text = orderCount.toString(),
@@ -68,8 +61,7 @@ fun CountItem(
                 .weight(1f),
             fontSize = 20.sp,
             textAlign = TextAlign.Center,
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
+            fontWeight = FontWeight.Bold
         )
         Surface(
             shape = RoundedCornerShape(size = 5.dp),
@@ -78,26 +70,21 @@ fun CountItem(
             contentColor = MaterialTheme.colorScheme.primary,
             modifier = Modifier.size(30.dp)
         ) {
-            Box(
-                contentAlignment = Alignment.Center,
-                modifier = modifier
-                    .fillMaxSize(1.0f)
-            ) {
-                Text(
-                    text = "＋",
-                    fontSize = 20.sp,
-                    modifier = Modifier
-                        .align(Alignment.Center)
-                        .clickable {
-                            onProductIncreased(orderId)
-                        },
-                    )
-            }
+            Text(
+                text = "＋",
+                fontSize = 22.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .weight(1f)
+                    .clickable {
+                        onProductIncreased(orderId)
+                    }
+            )
         }
     }
 }
 
-@Preview
+@Preview()
 @Composable
 fun ProductCounterPreview() {
     AmbatikTheme {
