792def34c30fde05d2fc807029861ec1d839e158
Austin Lieandro <72506433+austinlieandro@users.noreply.github.com>
2023-12-21 16:02:23 +0700

    Fix Article Screen

--- app/src/main/java/com/example/ambatik/ui/screen/articel/ArticelScreen.kt
+++ app/src/main/java/com/example/ambatik/ui/screen/articel/ArticelScreen.kt
@@ -28,6 +28,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
